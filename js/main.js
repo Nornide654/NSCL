1e6a5c4db289e6cd8ce51e32cbf92fb571b2f0e8
Nornide654 <milo1223345@gmail.com>
2026-06-08 16:53:57 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,21 @@
 import routes from './routes.js';
+
 export const store = Vue.reactive({
-    dark: JSON.parse(localStorage.getItem('dark')) ?? true,
-    darker: JSON.parse(localStorage.getItem('darker')) ?? false,
+    dark: JSON.parse(localStorage.getItem('dark')) || false,
     toggleDark() {
-        if (this.dark == true && this.darker == false) {
-            this.darker = true;
-        } else {
-            this.darker = false;
-            this.dark = !this.dark;
-        }
+        this.dark = !this.dark;
         localStorage.setItem('dark', JSON.stringify(this.dark));
-        localStorage.setItem('darker', JSON.stringify(this.darker));
     },
 });
+
 const app = Vue.createApp({
     data: () => ({ store }),
 });
 const router = VueRouter.createRouter({
-    history: VueRouter.createWebHistory(),
+    history: VueRouter.createWebHashHistory(),
     routes,
 });
+
 app.use(router);
 
 app.mount('#app');
