b482f0f5d8d5b0e54b72b42252d93e762aa795f2
Nornide654 <milo1223345@gmail.com>
2026-06-09 13:03:01 -0500

    Update score.js

--- js/score.js
+++ js/score.js
@@ -14,7 +14,7 @@ export function score(rank, percent, minPercent) {
     if (rank > 500) {
         return 0;
     }
-    if (rank > 150 && percent < 100) {
+    if (rank > 1000 && percent < 100) {
         return 0;
     }
 
